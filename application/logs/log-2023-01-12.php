0b41f0eb24f8333de209db0ba3c168d306d088e2
JeromeV21 <115931176+JeromeV21@users.noreply.github.com>
2023-01-12 22:32:50 +0700

    fix config

---
+++ application/logs/log-2023-01-12.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-01-12 03:23:19 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'project_db' C:\wamp64\www\VillaJudayResort\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2023-01-12 03:23:19 --> Unable to connect to the database
+ERROR - 2023-01-12 03:25:45 --> 404 Page Not Found: User/js
+ERROR - 2023-01-12 03:25:54 --> 404 Page Not Found: User/style.css
+ERROR - 2023-01-12 03:27:07 --> 404 Page Not Found: User/js
+ERROR - 2023-01-12 10:24:22 --> 404 Page Not Found: User/js
+ERROR - 2023-01-12 10:24:45 --> 404 Page Not Found: User/style.css
+ERROR - 2023-01-12 13:13:52 --> 404 Page Not Found: User/js
+ERROR - 2023-01-12 13:15:07 --> 404 Page Not Found: User/js
