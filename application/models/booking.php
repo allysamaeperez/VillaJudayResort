c100247625bb161a9be45d2c8ac893ecf25229ec
allysamaeperez <115536666+allysamaeperez@users.noreply.github.com>
2023-01-14 13:28:10 +0800

    Create booking.php

---
+++ application/models/booking.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Booking extends Model
+{
+    protected $table = 'booking';
+    protected $primaryKey = 'id';
+    
+    protected $allowedFields = [
+        'firstname',
+        'lastname',
+        'phonenumber',
+        'email',
+        'departuredate',
+        'arrivaldate',
+        'no_guest',
+        'roomtype'
+      
+    ];
+}
