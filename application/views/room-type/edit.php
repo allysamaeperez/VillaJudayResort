c337ccf106b3d3880bb8728a095833838631cf14
allysamaeperez <115536666+allysamaeperez@users.noreply.github.com>
2023-01-22 15:25:29 +0800

    Update edit.php

--- application/views/room-type/edit.php
+++ application/views/room-type/edit.php
@@ -2,26 +2,26 @@
 	
 	<div class="content clearfix">
 		
-		<form action="<?= base_url() ?>room_type/edit/<?=$room_type->room_type?>" method="post">
+		<form action="<?= base_url() ?>payment_type/edit/<?=$payment_type->payment_type?>" method="post">
 		
-			<h1>Update Room Type</h1>		
+			<h1>Update Payment Type</h1>		
 			
 			<div class="add-fields">
 
 				<div class="field">
-					<label for="room_type">Room Type:</label>
-					<input type="text" id="type" name="type" required value="<?=$room_type->room_type?>" placeholder="Room Type" readonly/>
+					<label for="payment_type">Payment Type:</label>
+					<input type="text" id="type" name="type" required value="<?=$payment_type->payment_type?>" placeholder="Payment Type" readonly/>
 				</div> <!-- /field -->
 				
 				<div class="field">
-					<label for="room_price">Price:</label>
-					<input type="number" min="1" id="price" name="price" required value="<?=$room_type->room_price?>" placeholder="Price"/>
+					<label for="price">Price:</label>
+					<input type="number" min="1" id="price" name="price" required value="<?=$payment_type->payment_price?>" placeholder="Price"/>
 					<i icon="icon-dollar"></i>
 				</div> <!-- /field -->
 
 				<div class="field">
-					<label for="room_details">Details:</label>
-					<input type="text" id="details" name="details" value="<?=$room_type->room_details?>" placeholder="Details of event"/>
+					<label for="details">Details:</label>
+					<input type="text" id="details" name="details" value="<?=$payment_type->payment_details?>" placeholder="Details"/>
 				</div> <!-- /field -->
 
 			</div> <!-- /login-fields -->
