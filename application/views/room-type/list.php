37eadcd778eca0fdce994b5552ac1f080829330e
allysamaeperez <115536666+allysamaeperez@users.noreply.github.com>
2023-01-22 15:27:14 +0800

    Update list.php

--- application/views/room-type/list.php
+++ application/views/room-type/list.php
@@ -3,12 +3,12 @@
     <div class="container">
       <div class="row">
         <div class="span12">
-			<a href="<?= base_url() ?>room-type/add" class="btn btn-small btn-primary"><i class="btn-icon-only icon-ok"></i>Add Room Type</a>
+			<a href="<?= base_url() ?>payment-type/add" class="btn btn-small btn-primary"><i class="btn-icon-only icon-ok"></i>Add Payment Type</a>
 			<br><br>
 			<table class="table table-striped table-bordered">
 				<thead>
 				  <tr>
-				    <th> Room Type </th>
+				    <th> Payment Type </th>
 				    <th> Price </th>
 				    <th> Details </th>
 				    <th> Quantity </th>
@@ -17,17 +17,17 @@
 				</thead>
 				<tbody>
 				<?php
-				if(isset($room_types) && is_array($room_types)):
-					foreach ($room_types as $rt) {
+				if(isset($payment_types) && is_array($payment_types)):
+					foreach ($payment_types as $rt) {
 				?>
 				  <tr>
-				    <td> <?=$rt->room_type ?> </td>
-				    <td> <?=$rt->room_price ?>$ </td>
-				    <td> <?=$rt->room_details ?> </td>
-				    <td> <?=$rt->room_quantity ?> </td>
+				    <td> <?=$rt->payment_type ?> </td>
+				    <td> <?=$rt->price ?>$ </td>
+				    <td> <?=$rt->details ?> </td>
+				    <td> <?=$rt->quantity ?> </td>
 				    <td class="td-actions">
-				    	<a href="<?= base_url() ?>room_type/edit/<?=$rt->room_type?>" class="btn btn-small btn-primary"><i class="btn-icon-only icon-edit"> </i></a>
-				    	<a href="<?= base_url() ?>room_type/delete/<?=$rt->room_type?>" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a>
+				    	<a href="<?= base_url() ?>payment_type/edit/<?=$rt->payment_type?>" class="btn btn-small btn-primary"><i class="btn-icon-only icon-edit"> </i></a>
+				    	<a href="<?= base_url() ?>payment_type/delete/<?=$rt->payment_type?>" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a>
 				    </td>
 				  </tr>
 				<?php } ?>
